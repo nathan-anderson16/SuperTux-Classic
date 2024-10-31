75dfd593bfeab89083ae22ab594e2a6f5fcc8d75
ctchartier <ctchartier@wpi.edu>
2024-10-31 11:28:28 -0400

    Better formatting, more info

--- SourceCode/autoload/logger.gd
+++ SourceCode/autoload/logger.gd
@@ -1,7 +1,6 @@
 extends Node
 
 var log_file_path: String = "" 
-onready var lives_text = $"/root/Scoreboard".lives_text
 
 func _ready():
 	log_file_path = OS.get_user_data_dir() + "/logs/log.txt"
@@ -11,6 +10,13 @@ func _ready():
 		dir.make_dir(OS.get_user_data_dir() + "/logs")
 	
 func write_log(message: String):
+	
+	var current_level_path = $"/root/Global".current_level_path
+	var lives_text = $"/root/Scoreboard".lives_text
+	var timer_text = $"/root/Scoreboard".timer_text
+	var coins_text = $"/root/Scoreboard".coins_text
+	var number_of_deaths = $"/root/Scoreboard".number_of_deaths
+	
 	var file = File.new()
 	
 	var error = file.open(log_file_path, File.READ_WRITE)
@@ -20,18 +26,40 @@ func write_log(message: String):
 		
 	file.seek_end()
 	var datetime = OS.get_datetime()
-	var micro = Time.get_unix_time_from_system()
-	var final_micro = str(micro).split(".")[1]
-	var timestamp = str(datetime.year) + "-" + str(datetime.month).pad_zeros(2) + "-" + str(datetime.day).pad_zeros(2) + " " + str(datetime.hour).pad_zeros(2) + ":" + str(datetime.minute).pad_zeros(2) + ":" + str(datetime.second).pad_zeros(2) + "." + final_micro
+	var micro = str(Time.get_unix_time_from_system()).split(".")[1]
+	var timestamp = str(datetime.year) + "-" + str(datetime.month).pad_zeros(2) + "-" + str(datetime.day).pad_zeros(2) + " " + str(datetime.hour).pad_zeros(2) + ":" + str(datetime.minute).pad_zeros(2) + ":" + str(datetime.second).pad_zeros(2) + "." + micro
 	file.store_line("[" + timestamp + "] " + message)
 	
+	var starter_message = "Level, Lives, Timer, Coin, Deaths"
 	var log_message = ""
 	
-	if lives_text:
-		log_message += " Lives Text: " + lives_text.text
+	if current_level_path:
+		log_message += str(current_level_path) + ","
 	else:
-		log_message += " Lives Text node is null"
+		log_message += "null,"
+		
+	if lives_text:
+		log_message += lives_text.text + ","
+	else:
+		log_message += "null,"
+		
+	if timer_text:
+		log_message += timer_text.text + ","
+	else:
+		log_message += "null,"
 	
+	if coins_text:
+		log_message += coins_text.text + ","
+	else:
+		log_message += "null,"
+	
+	if number_of_deaths:
+		log_message += str(number_of_deaths)
+	else:
+		log_message += str(0)
+
+	
+	file.store_line(starter_message)
 	file.store_line("[" + timestamp + "] " + log_message)
 	
 	file.close()

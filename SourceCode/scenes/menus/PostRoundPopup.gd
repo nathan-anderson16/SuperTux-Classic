4b015d0d0f7bd3950fa6f3a38878f288cb77c8fc
Nathan Anderson <101899832+nathan-anderson16@users.noreply.github.com>
2024-11-06 20:12:04 -0500

    Increase font size on post-round popup

--- SourceCode/scenes/menus/PostRoundPopup.gd
+++ SourceCode/scenes/menus/PostRoundPopup.gd
@@ -2,33 +2,40 @@ extends Popup
 
 signal test_popup_closed
 
+onready var qoe_submit_button = $QoEPopup/SubmitButton
+onready var acceptable_submit_button = $AcceptablePopup/AcceptableSubmitButton
+onready var acceptable_popup = $AcceptablePopup
+onready var qoe_popup = $QoEPopup
+onready var qoe_slider = $QoEPopup/QoeSlider
+onready var acceptable_no_button = $AcceptablePopup/NoButton
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$QoEPopup/SubmitButton.hide()
-	$AcceptablePopup/AcceptableSubmitButton.hide()
-	$AcceptablePopup.hide()
+	qoe_submit_button.hide()
+	acceptable_submit_button.hide()
+	acceptable_popup.hide()
 
 
 func _on_SubmitButton_pressed():
-	print("QoE: ", $QoEPopup/QoeSlider.value)
-	$QoEPopup.hide()
-	$AcceptablePopup.show()
+	print("QoE: ", qoe_slider.value)
+	qoe_popup.hide()
+	acceptable_popup.show()
 
 func _on_QoeSlider_gui_input(event):
 	# Only show the submit button when the slider is clicked
 	if event is InputEventMouseButton:
-		$QoEPopup/SubmitButton.show()
+		qoe_submit_button.show()
 
 
 func _on_AcceptableSubmitButton_pressed():
-	print("Acceptable: ", "No" if $AcceptablePopup/HBoxContainer/NoButton.pressed else "Yes")
+	print("Acceptable: ", "No" if acceptable_no_button.pressed else "Yes")
 	self.hide()
 	emit_signal("test_popup_closed")
 
 
 func _on_NoButton_pressed():
-	$AcceptablePopup/AcceptableSubmitButton.show()
+	acceptable_submit_button.show()
 
 
 func _on_YesButton_pressed():
-	$AcceptablePopup/AcceptableSubmitButton.show()
+	acceptable_submit_button.show()

220219b6640d833814c751cde07b3f9affb11c66
Nathan Anderson <101899832+nathan-anderson16@users.noreply.github.com>
2024-11-19 17:18:28 -0500

    Reset checkpoint is now triggered when the player is below 100 velocity

--- SourceCode/scenes/objects/ResetCheckpoint.gd
+++ SourceCode/scenes/objects/ResetCheckpoint.gd
@@ -2,8 +2,19 @@ extends Node2D
 
 onready var sfx = $SFX
 onready var animation_player = $AnimationPlayer
+onready var hitbox = $Area2D
+
+func _process(delta):
+	if hitbox.overlaps_body(Global.player):
+		var player_velocity = sqrt(pow(Global.player.velocity.x, 2) + pow(Global.player.velocity.y, 2))
+		if player_velocity < 100:
+			Logger.log_event("Success: Reset Checkpoint Reached")
+			Scoreboard.add_score(100)
+			print("Score: ", Scoreboard.score)
+			Global.reset_level()
 
 func _on_Area2D_body_entered(body):
-	Scoreboard.add_score(100)
-	print("Score: ", Scoreboard.score)
-	Global.reset_level()
+	pass
+#	Scoreboard.add_score(100)
+#	print("Score: ", Scoreboard.score)
+#	Global.reset_level()

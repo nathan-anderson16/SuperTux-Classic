6fd9bd85f1099b770425bb39b8951fc40999105d
ctchartier <ctchartier@wpi.edu>
2024-12-04 23:27:35 -0500

    Calculate the spike in real time

--- SourceCode/scenes/objects/RandomDelayLagHitbox.gd
+++ SourceCode/scenes/objects/RandomDelayLagHitbox.gd
@@ -2,23 +2,36 @@ extends Area2D
 
 onready var sprite = get_node_or_null("Sprite")
 
+var entry_times = {}
+
 func _ready():
 	if sprite: sprite.hide()
 
 func _on_Lava_body_entered(body):
+	var enter_time = OS.get_ticks_msec() / 1000.0 
+	entry_times[body] = enter_time
 	# If the body is invincible, don't kill it
 #	if body.get("invincible"):
 #		if body.invincible == true: return
 	if body.has_method("enter_delay_lag_field"):
 		body.enter_delay_lag_field()
-		Logger.log_event("Random Delay Enter")
+		Logger.log_event("Random Delay Enter: Entered at time: %f" % enter_time)
 		return
 
 func _on_Lava_body_exited(body):
+	if not entry_times.has(body):
+		return
+	
 	# If the body is invincible, don't kill it
 #	if body.get("invincible"):
 #		if body.invincible == true: return
+
+	var exit_time = OS.get_ticks_msec() / 1000.0  
+	var enter_time = entry_times[body]
+	var duration = exit_time - enter_time
+	
+	entry_times.erase(body)
 	if body.has_method("exit_delay_lag_field"):
 		body.exit_delay_lag_field()
-		Logger.log_event("Random Delay Exit")
+		Logger.log_event("Random Delay Exit: Exited at time: %f, Duration: %f seconds" % [exit_time, duration])
 		return

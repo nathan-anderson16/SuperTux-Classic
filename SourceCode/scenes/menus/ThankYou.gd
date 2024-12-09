cfe9403e2acc4a163f440bc738167369e980cdfe
Nathan Anderson <101899832+nathan-anderson16@users.noreply.github.com>
2024-12-09 18:49:50 -0500

    Create summary log when the "Thank You" screen is shown

--- SourceCode/scenes/menus/ThankYou.gd
+++ SourceCode/scenes/menus/ThankYou.gd
@@ -4,6 +4,7 @@ onready var score_label = $TitleContent/Panel/Score
 
 func _ready():
 	score_label.text = "Score: %s" % Scoreboard.score
+	Logger.create_summary_log()
 
 func _on_CloseButton_pressed():
 	Global.goto_title_screen()

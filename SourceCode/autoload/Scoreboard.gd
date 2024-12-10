6dff40805e80fc545af1fd14075e57cab4c92296
Nathan Anderson <101899832+nathan-anderson16@users.noreply.github.com>
2024-12-09 19:06:04 -0500

    Make the staircase at the end of the practice level better

--- SourceCode/autoload/Scoreboard.gd
+++ SourceCode/autoload/Scoreboard.gd
@@ -96,9 +96,9 @@ func _process(delta):
 				if level_timer.time_left <= 40 and (Global.spawn_position == null or Global.spawn_position.x < 4112) :
 					# Teleport to 7 Jump on next death
 					Global.spawn_position = Vector2(4112, 112)
-				if level_timer.time_left <= 20 and (Global.spawn_position == null or Global.spawn_position.x < 5584) :
+				if level_timer.time_left <= 20 and (Global.spawn_position == null or Global.spawn_position.x < 4896) :
 					# Teleport to Z Jump on next death
-					Global.spawn_position = Vector2(5584, 112)
+					Global.spawn_position = Vector2(4896, 112)
 		# If we have under 10 seconds remaining in the current level:
 		if level_timer.time_left < 10:
 			# Play a clock ticking noise every second

e234478076982abfd392444b64b352bdfdfe630d
Nathan Anderson <101899832+nathan-anderson16@users.noreply.github.com>
2025-04-21 22:42:03 -0400

    Show correct score on thank you screen

--- SourceCode/scenes/menus/ThankYou.gd
+++ SourceCode/scenes/menus/ThankYou.gd
@@ -3,7 +3,7 @@ extends Control
 onready var score_label = $TitleContent/Panel/Score
 
 func _ready():
-	score_label.text = "Score: %s" % Scoreboard.score
+	score_label.text = "Score: %s" % (Scoreboard.score + Scoreboard.bonus_score())
 	Logger.create_summary_log()
 
 func _on_CloseButton_pressed():
